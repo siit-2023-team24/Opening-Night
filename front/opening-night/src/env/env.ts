c3f16052bf7eef484330ccb9bc8c17bbdbd984f2
Sonja-Parabucki <105170152+Sonja-Parabucki@users.noreply.github.com>
2024-07-08 22:16:52 +0200

    delete

--- front/opening-night/src/env/env.ts
+++ front/opening-night/src/env/env.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiHost: "https://e2hivkdapc.execute-api.eu-central-1.amazonaws.com/prod"
+    apiHost: "https://koz8hyjt90.execute-api.eu-central-1.amazonaws.com/prod"
 }
