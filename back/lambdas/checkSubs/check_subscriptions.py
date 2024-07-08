e06fc4758970c134f2b8af8001a37b2d10cace2b
Sonja-Parabucki <105170152+Sonja-Parabucki@users.noreply.github.com>
2024-07-09 01:20:52 +0200

    sns fix

--- back/lambdas/checkSubs/check_subscriptions.py
+++ back/lambdas/checkSubs/check_subscriptions.py
@@ -19,33 +19,42 @@ def check(event, context):
         print(record)
         new_image = record['dynamodb']['NewImage']
         title = new_image.get('title', '').get('S', '')
-        genres = new_image.get('genres', {}).get('SS', [])
-        for genre in genres:
+        genres = new_image.get('genres', {}).get('L', [])
+        for g in genres:
+            genre = g.get('S', '')
+            print(g)
             message = f'A new {genre} film "{title}" just arrived!'
             pub('g' + genre, message)
 
-        directors = new_image.get('directors', {}).get('SS', [])
-        for director in directors:
+        directors = new_image.get('directors', {}).get('L', [])
+        for d in directors:
+            print(d)
+            director = d.get('S', '')
             message = f'A new film "{title}" by {director} just arrived!'
             pub('d' + director, message)
 
-        actors = new_image.get('actors', {}).get('SS', [])
-        for actor in actors:
+        actors = new_image.get('actors', {}).get('L', [])
+        for a in actors:
+            print(a)
+            actor = a.get('S', '')
             message = f'A new film "{title}" starring {actor} just arrived!'
             pub('a' + actor, message)
 
         #writes to sqs
         for sub in subs:
             found_intersection = False
-            for genre in genres:
+            for g in genres:
+                genre = g.get('S', '')
                 if genre in sub['genres']:
                     found_intersection = True
                     break
-            for director in directors:
+            for d in directors:
+                director = d.get('S', '')
                 if director in sub['directors']:
                     found_intersection = True
                     break
-            for actor in actors:
+            for a in actors:
+                actor = a.get('S', '')
                 if actor in sub['actors']:
                     found_intersection = True
                     break
@@ -59,6 +68,8 @@ def check(event, context):
 
 
 def pub(topic_name, message):
+    topic_name = topic_name.replace(' ', '_')
+    print(topic_name)
     response = sns.create_topic(Name=topic_name)
     topic_arn = response['TopicArn']
 
