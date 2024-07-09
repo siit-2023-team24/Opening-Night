9602f5c54936b81bd894d60dea38b1bca247aa99
Sonja-Parabucki <105170152+Sonja-Parabucki@users.noreply.github.com>
2024-07-09 05:06:12 +0200

    download

--- back/lambdas/downloadFilm/download_film.py
+++ back/lambdas/downloadFilm/download_film.py
@@ -1,7 +1,13 @@
 import os
 import boto3
 import base64
+import json
+from datetime import datetime
+
 s3_client = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+sqs = boto3.client('sqs')
+
 def get_one(event, context):
     
     bucket_name = os.environ['BUCKET_NAME']
@@ -13,6 +19,35 @@ def get_one(event, context):
     response = s3_client.get_object(Bucket=bucket_name, Key=name)
     content = response['Body'].read()
     encoded_content = base64.b64encode(content)
+
+    event = json.loads(event['body'])
+    filmId = event['filmId']
+    username = event['username']
+    genres = event['genres']
+    actors = event['actors']
+    directors = event['directors']
+    timestamp = datetime.now().isoformat()
+
+
+    table_name = os.environ['DOWNLOADS_TABLE_NAME']
+    table = dynamodb.Table(table_name)
+    table.put_item(
+        Item = {
+            'username': username,
+            'timestamp': timestamp,
+            'filmId': filmId,
+            'actors': actors,
+            'directors': directors,
+            'genres': genres
+        }
+    )
+
+    queue_url = os.environ['CUSTOM_VAR']
+    sqs.send_message(
+        QueueUrl=queue_url,
+        MessageBody=username
+    )
+
     
     return {
         'statusCode': 200,
